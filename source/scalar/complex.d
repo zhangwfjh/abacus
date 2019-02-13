1aa4b920163bcaea88b3ffa89c01faff6705eac7
NeilKleistGao <NeilKleistGao@163.com>
2019-02-13 19:43:42 +0800

    new

--- source/scalar/complex.d
+++ source/scalar/complex.d
@@ -1,5 +1,6 @@
 module scalar.complex;
 
+import scalar.decimal : Float;
 import std.math : sqrt;
 import std.format;
 
@@ -11,15 +12,7 @@ nbits : the number of bits for each real component
 +/
 struct Complex(size_t nbits) 
 {
-    static if (nbits == 16)
-        alias Real = float;
-    else static if (nbits == 32)
-        alias Real = double;
-    else static if (nbits == 64)
-        alias Real = real;
-    else
-        static assert(false, "The type is not supported");
-
+    alias Real = scalar.decimal.Float!nbits;
     Real re, im;
 
     @safe @nogc pure nothrow this(Real _re, Real _im)
@@ -45,46 +38,40 @@ struct Complex(size_t nbits)
 
     @safe pure Complex inverse() const @property
     {
-        if (re != 0 || im != 0)
-            return Complex(re / (re^^2 + im^^2), -im / (re^^2 + im^^2));
-        else
-            assert(false, "The zero value doesn't have inverse");
+        return Complex(re / (re^^2 + im^^2), -im / (re^^2 + im^^2));
     }
 
     @safe Complex opBinary(string op)(Complex other) const
     {
-        if (op == "+")
+        static if (op == "+")
             return Complex(re + other.re, im + other.im);
-        else if (op == "-")
+        else static if (op == "-")
             return Complex(re - other.re, im - other.im);
-        else if (op == "*")
+        else static if (op == "*")
             return Complex(re * other.re - im * other.im, re * other.im + im * other.re);
-        else if (op == "/")
+        else static if (op == "/")
             return Complex((re * other.re + im * other.im) / other.abs^^2, (im * other.re - re * other.im) / other.abs^^2);
+        else static if (op == "^^")
+            ///TODO
         else
-            assert(false, "The type is not supported");
+            static assert(false, "The type is not supported");
     }
 
-    @safe Complex opUnary(string op)()
+    @safe Complex opUnary(string op)() const
     {
-        if (op == "+")
+        static if (op == "+")
             return this;
-        else if (op == "-")
+        else static if (op == "-")
             return Complex(-re, -im);
-        else if (op == "~")
+        else static if (op == "~")
             return this.conjugate;
         else
-            assert(false, "The type is not supported");
+            static assert(false, "The type is not supported");
     }
 
-    @safe bool equal(Complex other) const
+    @safe bool opEquals(Complex other) const
     {
         return re == other.re && im == other.im;
-    }
-
-    @safe bool equal(Real other) const
-    {
-        return re == other && im == 0;
     }
 
     @safe Real distance(Complex other) const
